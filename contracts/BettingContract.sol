5f4c7b7ee7708f4562a9a537f42f2f44f77eea05
Dominique Buob <82541879+HighDom@users.noreply.github.com>
2024-09-08 15:41:47 +0200

    feat: Add BettingContract feat: Joinbet Handler

--- contracts/BettingContract.sol
+++ contracts/BettingContract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {FunctionsClient} from "@chainlink/contracts@1.2.0/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
-import {ConfirmedOwner} from "@chainlink/contracts@1.2.0/src/v0.8/shared/access/ConfirmedOwner.sol";
-import {FunctionsRequest} from "@chainlink/contracts@1.2.0/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 
 contract Betting is FunctionsClient, ConfirmedOwner {
     using FunctionsRequest for FunctionsRequest.Request;
