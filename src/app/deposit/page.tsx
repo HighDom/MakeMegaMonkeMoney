8d314d01a727c507fa30ab52d89587f5d4936208
Dominique Buob <82541879+HighDom@users.noreply.github.com>
2024-09-14 10:02:41 +0200

    feat: withdraw

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -1,13 +1,15 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import DepositMoney from "@/components/DepositMoney/DepositMoney";
+import WithdrawMoney from "@/components/DepositMoney/WithdrawMoney";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 const deposit = () => {
   return (
     <DefaultLayout>
-      <div className="mx-auto max-w-270">
+      <div className="mx-auto max-w-270 space-y-10">
         <Breadcrumb pageName="Deposit" />
         <DepositMoney />
+        <WithdrawMoney />
       </div>
     </DefaultLayout>
   );
--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -2,14 +2,22 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import { useWeb3Auth } from "@/context/useWeb3Auth";
+import { getBalance } from "viem/actions";
 
 const Introduction: React.FC = () => {
   const router = useRouter();
-  const { loggedIn } = useWeb3Auth();
+  const { loggedIn, userBalance } = useWeb3Auth();
+
+  const validUserBalance =
+    !userBalance ||
+    isNaN(parseFloat(userBalance)) ||
+    parseFloat(userBalance) <= 0
+      ? false
+      : true;
 
   return (
-    <div className="mx-auto max-w-2xl space-y-8 p-6">
-      <h1 className="mb-6 text-center text-3xl font-bold text-green-900">
+    <div className=" max-w-2xl space-y-8 p-6">
+      <h1 className="mb-6 text-3xl font-bold text-green-900">
         Welcome to MakeMegaMonkeMoney
       </h1>
 
@@ -48,22 +56,44 @@ const Introduction: React.FC = () => {
         </div>
       )}
 
-      <div className="flex justify-center space-x-4 pt-4">
-        <button
-          onClick={() => router.push("/bet")}
-          disabled={!loggedIn}
-          className="inline-block w-full rounded-lg bg-primary px-10 py-4 text-center font-medium text-white transition-all duration-300 hover:bg-opacity-90 dark:bg-primary dark:text-white dark:hover:bg-opacity-80"
+      {!validUserBalance && (
+        <div
+          className="border-l-4 border-yellow-500 bg-yellow-100 p-4 text-yellow-700"
+          role="alert"
         >
-          Create Bet
-        </button>
-        <button
-          onClick={() => router.push("/joinBet")}
-          disabled={!loggedIn}
-          className="inline-block w-full rounded-lg bg-primary px-10 py-4 text-center font-medium text-white transition-all duration-300 hover:bg-opacity-90 dark:bg-primary dark:text-white dark:hover:bg-opacity-80"
-        >
-          Join Bet
-        </button>
-      </div>
+          <p className="font-bold">Please Note:</p>
+          <p>
+            You need a balance greater than 0 to create or join bets. Please
+            deposit funds
+          </p>
+          <button
+            onClick={() => router.push("/deposit")}
+            disabled={!loggedIn}
+            className="mt-5 inline-block w-full rounded-lg bg-primary px-10 py-4 text-center font-medium text-white transition-all duration-300 hover:bg-opacity-90 dark:bg-primary dark:text-white dark:hover:bg-opacity-80"
+          >
+            Deposit Money
+          </button>
+        </div>
+      )}
+
+      {loggedIn && validUserBalance && (
+        <div className="flex justify-center space-x-4 pt-4">
+          <button
+            onClick={() => router.push("/bet")}
+            disabled={!loggedIn}
+            className="inline-block w-full rounded-lg bg-primary px-10 py-4 text-center font-medium text-white transition-all duration-300 hover:bg-opacity-90 dark:bg-primary dark:text-white dark:hover:bg-opacity-80"
+          >
+            Create Bet
+          </button>
+          <button
+            onClick={() => router.push("/joinBets")}
+            disabled={!loggedIn}
+            className="inline-block w-full rounded-lg bg-primary px-10 py-4 text-center font-medium text-white transition-all duration-300 hover:bg-opacity-90 dark:bg-primary dark:text-white dark:hover:bg-opacity-80"
+          >
+            Join Bet
+          </button>
+        </div>
+      )}
     </div>
   );
 };
